1f2c5f6ade2bffea5a9842c7d109ba9d762d06b0
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2024-10-03 16:44:26 +0800

    [refactor] use interface.report to handle output

--- battle.py
+++ battle.py
@@ -12,27 +12,27 @@ class Battle:
 
     def battle_start(self):
         if self.room.all_enemies_defeated():
-            print("All soldiers in the room have been defeated already!")
+            interface.report("All soldiers in the room have been defeated already!")
             return
-        print("YOU FOUND ENEMIES!!!!")
+        interface.report("YOU FOUND ENEMIES!!!!")
         while not self.battle_over():
             attacker = self.player
             defender = self.room.get_enemies()[0]
             self.attack(attacker, defender)
             if defender.isdead():
-                print(interface.death_msg(defender.get_type()))
+                interface.report(interface.death_msg(defender.get_type()))
                 if attacker == self.player:
                     self.room.remove_enemy()
                     defender = self.room.get_enemies()[0]
             attacker, defender = defender, attacker
             time.sleep(0.1)
         if self.room.all_enemies_defeated():
-            print("All soldiers in the room are defeated!")
-        print("Battle over!")
+            interface.report("All soldiers in the room are defeated!")
+        interface.report("Battle over!")
 
     def attack(self, attacker, defender) -> None:
         defender.receive_damage(attacker.damage)
-        print(self.attack_report(attacker, defender))
+        interface.report(self.attack_report(attacker, defender))
 
     def attack_report(self, attacker, defender) -> str:
         if attacker.get_type() == "Player":
