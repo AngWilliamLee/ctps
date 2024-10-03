14cc8db44d433915f8a0d6fa5ee3ab2e208fba23
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2024-10-03 16:42:37 +0800

    [refactor] Use report() to handle output

--- interface.py
+++ interface.py
@@ -9,12 +9,12 @@ report = print
 
 def prompt_choice(message: str, options: list[str], prompt: str) -> str:
     """Helper function to prompt the user for a choice."""
-    print(message)
+    report(message)
     for i, value in enumerate(options, start=1):
-        print(f"{i}: {value}")
+        report(f"{i}: {value}")
     choice = input(prompt)
     while not choice.isdecimal() or int(choice) - 1 not in range(len(options)):
-        print("Invalid choice.")
+        report("Invalid choice.")
         choice = input(prompt)
     return options[int(choice) - 1]
 
@@ -25,8 +25,8 @@ def prompt():
 
 
 def combat_menu(player_health, enemy_health):
-    print(f"Your health: {player_health}")
-    print(f"Enemy health: {enemy_health}")
+    report(f"Your health: {player_health}")
+    report(f"Enemy health: {enemy_health}")
 
 
 def prompt_menu(room_name: str) -> str:
@@ -34,7 +34,7 @@ def prompt_menu(room_name: str) -> str:
 
 
 def exit_screen():
-    print(script.exit_screen['message'])
+    report(script.exit_screen['message'])
 
 
 def death_msg(char_type: str) -> str:
@@ -48,8 +48,8 @@ def death_msg(char_type: str) -> str:
 
 
 def win_msg():
-    print(script.win_msg)
+    report(script.win_msg)
 
 
 def caught_msg():
-    print(script.caught_msg)
+    report(script.caught_msg)
