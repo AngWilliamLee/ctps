a29a7d1df80a33f2a42ea3c1c7cc1ea8639a78b7
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2024-10-03 07:11:02 +0000

    [refactor] Eliminate Interface class

--- ctps.py
+++ ctps.py
@@ -6,7 +6,6 @@ import battle
 class Game:
 
     def __init__(self):
-        self.interface = interface.Interface()
         self.player = None
         self.princess = None
         self.rooms = []
@@ -16,19 +15,19 @@ class Game:
         self.player = data.createPlayer()
         self.rooms = data.createRooms()
         self.princess = self.rooms[-1].get_enemies()[-1]
-        if self.interface.prompt_menu('start') == 'exit':
-            self.interface.exit_screen()
+        if interface.prompt_menu('start') == 'exit':
+            interface.exit_screen()
             exit()
 
     def isover(self):
         if self.player.isdead():
-            self.interface.death_msg()
+            interface.death_msg()
             return True
         elif self.princess.isdead():
             if all([room.all_enemies_defeated() for room in self.rooms]):
-                self.interface.win_msg()
+                interface.win_msg()
             else:
-                self.interface.caught_msg()
+                interface.caught_msg()
             return True
         return False
 
@@ -61,7 +60,7 @@ class Game:
     def get_choice(self):
         "Dispalys and gets player choice. Display results afterwards"
         room_name = self.get_now_room_name().lower()
-        choice = self.interface.prompt_menu(room_name)
+        choice = interface.prompt_menu(room_name)
         print(choice + '\n')
         if choice == 'Move to next room':
             self.next_room()
--- interface.py
+++ interface.py
@@ -14,26 +14,31 @@ def prompt_choice(message: str, options: list[str], prompt: str) -> str:
     return options[choice - 1]
 
 
-class Interface:
 
-    def prompt(self):
-        return script.prompt
+def prompt():
+    return script.prompt
 
-    def combat_menu(self, player_health, enemy_health):
-        print(f"Your health: {player_health}")
-        print(f"Enemy health: {enemy_health}")
 
-    def prompt_menu(self, room_name: str) -> str:
-        return prompt_choice(script.menu[room_name]['message'], script.menu[room_name]["options"], script.prompt)
+def combat_menu(player_health, enemy_health):
+    print(f"Your health: {player_health}")
+    print(f"Enemy health: {enemy_health}")
 
-    def exit_screen(self):
-        print(script.exit_screen['message'])
 
-    def death_msg(self):
-        print(script.death_msg)
+def prompt_menu(room_name: str) -> str:
+    return prompt_choice(script.menu[room_name]['message'], script.menu[room_name]["options"], script.prompt)
 
-    def win_msg(self):
-        print(script.win_msg)
 
-    def caught_msg(self):
-        print(script.caught_msg)
+def exit_screen():
+    print(script.exit_screen['message'])
+
+
+def death_msg():
+    print(script.death_msg)
+
+
+def win_msg():
+    print(script.win_msg)
+
+
+def caught_msg():
+    print(script.caught_msg)
