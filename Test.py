fe8fc2b55502602e08f367a63bd6297883b474a8
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2024-10-04 00:40:04 +0000

    [test] Set interface to log to file during testing

--- Test.py
+++ Test.py
@@ -1,1 +1,10 @@
 import interface
+
+LOGFILE = "test.log"
+
+def log_to_file(message: str) -> None:
+    with open(LOGFILE, "a") as f:
+        f.write(message + "\n")
+
+interface.report = log_to_file
+
